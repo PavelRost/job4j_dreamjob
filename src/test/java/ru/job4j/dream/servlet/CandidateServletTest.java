6f7b65308e3d1ce8dac4a2bea999b4c0e69a4715
Rostmp <84409690+Rostmp@users.noreply.github.com>
2022-02-10 19:23:47 +0300

    1. Написать тест на PostServlet. [#2518 #262163]

--- src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -4,17 +4,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.DbStore;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class CandidateServletTest {
@@ -33,6 +35,18 @@ public class CandidateServletTest {
                 .get();
         assertThat(candidate, notNullValue());
         assertThat(candidate.getName(), is("name of new candidate"));
+    }
+
+    @Test
+    public void whenGetResponseSuccess() throws ServletException, IOException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getRequestDispatcher("candidates.jsp")).thenReturn(dispatcher);
+        new CandidateServlet().doGet(req, resp);
+        verify(req).getRequestDispatcher("candidates.jsp");
     }
 
 }
--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -2,14 +2,17 @@ package ru.job4j.dream.servlet;
 
 import static org.junit.Assert.*;
 
+import org.h2.engine.Session;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.DbStore;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,8 +20,7 @@ import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class PostServletTest {
@@ -40,13 +42,16 @@ public class PostServletTest {
         assertThat(post.getName(), is("name of new post"));
     }
 
-    @Ignore
     @Test
     public void whenGetResponseSuccess() throws ServletException, IOException {
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-        req.setAttribute("posts", new ArrayList<>(DbStore.instOf().findAllPosts()));
+        HttpSession session = mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getRequestDispatcher("posts.jsp")).thenReturn(dispatcher);
         new PostServlet().doGet(req, resp);
+        verify(req).getRequestDispatcher("posts.jsp");
     }
 
 }
